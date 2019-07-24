69cfc73afb845e1f050f6d3e07580837b60328f6
mbulano1 <mbulano1@ngs.ru>
2019-07-24 14:31:51 +0300

    очень непростые задания. выполнил как мог :((( задания без модуля re еще не смог сделать, даже подсмотрев урок с разбором.
    Еще не понял разницы в re между скобками () и такими [] скобками.:( посмотрел кучу материалов в гугле и ютьюбе, но там все проще, чем это задание

---
+++ lesson 4 norm.py
@@ -0,0 +1,166 @@
+# Задание-1:
+# Вывести символы в нижнем регистре, которые находятся вокруг
+# 1 или более символов в верхнем регистре.
+# Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
+# Решить задачу двумя способами: с помощью re и без.
+
+line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
+       'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
+       'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn'\
+       'LxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIVjXa'\
+       'pzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnWete'\
+       'kUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfCvzQ'\
+       'WrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbRuXb'\
+       'JrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkmjCC'\
+       'EUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOnLfB'\
+       'tQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGSeuT'\
+       'SkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCfKCu'\
+       'UJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWHuXB'\
+       'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'\
+       'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
+       'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+
+import re
+
+textlookfor = r'[a-z]+'
+allresults = re.findall(textlookfor,line)
+print(allresults)
+
+
+line2 = list(i for i in line if i  != i.capitalize())
+print(line2)
+
+# Задание-2:
+# Вывести символы в верхнем регистре, слева от которых находятся
+# два символа в нижнем регистре, а справа - два символа в верхнем регистре.
+# Т.е. из строки
+# "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
+# нужно получить список строк: ['AY', 'NOGI', 'P']
+# Решить задачу двумя способами: с помощью re и без.
+
+line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
+       'NOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewV'\
+       'fzKTUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSA'\
+       'HqnLxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIV'\
+       'jXapzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnW'\
+       'etekUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfC'\
+       'vzQWrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbR'\
+       'uXbJrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkm'\
+       'jCCEUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOn'\
+       'LfBtQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGS'\
+       'euTSkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCf'\
+       'KCuUJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWH'\
+       'uXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQN'\
+       'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
+       'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+
+lookingfor = r'[a-z]{2}([A-Z]+)[A-Z]{2}'
+print(str(re.findall(lookingfor, line_2)))
+
+# Задание-3:
+# Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
+# произвольными целыми цифрами, в результате в файле должно быть
+# 2500-значное произвольное число.
+# Найдите и выведите самую длинную последовательность одинаковых цифр
+# в вышезаполненном файле.
+
+
+import random
+import re
+
+
+x = random.randint(10**2499, 10**2500)
+print(x)
+print(len(str(x)))
+
+outputfile = "../myfile.txt"
+myfile = open(outputfile, mode='w')
+myfile.write(str(x))
+myfile.close()
+
+all = []
+
+myfile = open(outputfile, mode ='r')
+lookingfor = r'(9+)'
+nine = re.findall(lookingfor, myfile.read())
+myfile.close()
+
+all.append(nine)
+
+myfile = open(outputfile, mode ='r')
+lookingfor = r'(8+)'
+eigth = re.findall(lookingfor, myfile.read())
+myfile.close()
+
+all.append(eigth)
+
+myfile = open(outputfile, mode ='r')
+lookingfor = r'(7+)'
+seven = re.findall(lookingfor, myfile.read())
+myfile.close()
+
+all.append(seven)
+
+myfile = open(outputfile, mode ='r')
+lookingfor = r'(6+)'
+six = re.findall(lookingfor, myfile.read())
+myfile.close()
+
+all.append(six)
+
+myfile = open(outputfile, mode ='r')
+lookingfor = r'(5+)'
+five = re.findall(lookingfor, myfile.read())
+myfile.close()
+
+all.append(five)
+
+myfile = open(outputfile, mode ='r')
+lookingfor = r'(4+)'
+four = re.findall(lookingfor, myfile.read())
+myfile.close()
+
+all.append(four)
+
+myfile = open(outputfile, mode ='r')
+lookingfor = r'(3+)'
+three = re.findall(lookingfor, myfile.read())
+myfile.close()
+
+all.append(three)
+
+myfile = open(outputfile, mode ='r')
+lookingfor = r'(2+)'
+two = re.findall(lookingfor, myfile.read())
+myfile.close()
+
+all.append(two)
+
+myfile = open(outputfile, mode ='r')
+lookingfor = r'(1+)'
+one = re.findall(lookingfor, myfile.read())
+myfile.close()
+
+all.append(one)
+
+myfile = open(outputfile, mode ='r')
+lookingfor = r'(0+)'
+zero = re.findall(lookingfor, myfile.read())
+myfile.close()
+
+all.append(zero)
+
+print(all)
+
+
+
+max = len(all[0][0])
+maxstr = all[0][0]
+print(max, maxstr)
+
+for i in all:
+    for j in i:
+     if max < len(j):
+        maxstr = len(j)
+        maxstr = j
+print(max, maxstr)
